44ccbcea0067a03159a89776e2f6861aeb5a1c6e
BionicHawk <angel.manzo.rosas@gmail.com>
2024-07-19 01:46:39 -0600

    Methods used for socket communitaction defined

--- android/app/src/main/kotlin/com/bionichawk/pc_controller/MainActivity.kt
+++ android/app/src/main/kotlin/com/bionichawk/pc_controller/MainActivity.kt
@@ -3,4 +3,17 @@ package com.bionichawk.pc_controller
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    private fun openSocketChannel() {
+
+    }
+
+    private fun closeSocketChannel() {
+
+    }
+
+    private fun sendDate(data: String) {
+
+    }
+
 }
